14d61ceae1338f66986614fb7b8cc3d8be4be056
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-23 18:59:47 +0330

    3bahman

---
+++ 2.py
@@ -0,0 +1,68 @@
+# x = 1
+# print(type(x))
+# نامگذاری متغیرهای در پایتون
+# هر تر کیبی از کاراکترهای a-z و اعداد و _
+# number = 1
+# number1 = 1
+# number_1 = 3
+# nu2m = 2
+# 2num = 3 Error
+# _number = 2
+# number one = 2 # error
+
+# name = 'nima' # "nima"
+# print(type(name))
+
+
+# number1 = 1
+# number2 = 2
+
+# result = number1 + number2
+
+# print("sum of two numbers is:", result)
+
+
+# result = number1 - number2
+# print('sub of two numbers is:', result)
+
+# result = number1 * number2
+# print('mul of two numbers is:', result)
+
+# result = number1/number2
+# print("div of two numbers is:", result)
+
+
+# name1 = "soheil"
+# name2 = "iliya"
+# print(name1)
+# print(name2)
+
+name = "sepehr"
+family = "rezaei"
+print(name, family)
+
+result = name + ' ' + family
+print(result)
+
+# res = 1 + 'ali' # TypeError
+
+print('#'*5)
+# print('#'*'#')  # TypeError
+
+n = 'c'
+print(type(n))
+x = 'ccccc'
+print(type(x))
+# - and / are not defined on str
+name = "pendar"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+
+# exercise 1 : output : a b c d e f g h
+# exercise 2 : output : **********     10 ta star
+# exercise 3 : کاراکترهای اسمتون رو توی هر خط بنویسیو
