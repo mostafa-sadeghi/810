d0a6efc475768025a53e2aa99dd866f186113b77
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-01 18:59:33 +0330

    sadeghi

---
+++ about_list2.py
@@ -0,0 +1,43 @@
+# numbers = []
+# print('before adding', numbers)
+# numbers.append(1)
+# numbers.append(2)
+# numbers.append(3)
+# numbers.append(4)
+# print('after adding', numbers)
+
+# print('first item:', numbers[0])
+# print('last item:', numbers[3])
+# print('last item with python magic:', numbers[-1])
+
+# print('list size:', len(numbers))
+
+
+# slicing on list
+
+names = ['martin', 'sara', 'radin', 'nikan']
+
+my_slice = names[1:3]
+print(my_slice)
+
+second_slice = names[2:]
+print(second_slice)
+
+string = 'abcdefgh'
+
+print(string[3:6])
+
+# exercise 1 : از string
+'''
+برش های زیر را نمایش دهید
+cde
+fg
+ef
+'''
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(numbers[::2])
+
+#  exercise 2 : numbers = [1,2,3,4,5,6,7]
+'''
+یک برش از اعداد زوج ایجاد کنید
+'''
