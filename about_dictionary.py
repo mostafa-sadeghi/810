4fb775ccc0319b1125d73c90095704955a795738
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-13 18:59:23 +0330

    sadeghi

---
+++ about_dictionary.py
@@ -0,0 +1,46 @@
+# favorite_sports = {}
+# print(type(favorite_sports))
+
+# favorite_sports = {
+#     'sepehr': 'football',
+#     'pendar': 'basketball',
+#     'nika': 'tennis',
+#     'iliya': 'football',
+#     'atay': 'pingpong',
+
+# }
+# print(favorite_sports['iliya'])
+# print(favorite_sports['sepehr'])
+# print(favorite_sports['atay'])
+
+
+# print(favorite_sports.items())
+# print(favorite_sports.keys())
+# print(favorite_sports.values())
+
+favorite_sports = {
+    'sepehr': 'football',
+    'pendar': 'basketball',
+    'nika': 'tennis',
+    'iliya': 'football',
+    'atay': 'pingpong',
+    'some one': "some thing"
+
+}
+
+favorite_sports['nima'] = "football"
+
+print("after adding ...", favorite_sports)
+
+
+del favorite_sports['some one']
+
+print("after removing", favorite_sports)
+
+#   برنامه ای بنویسید که نام و ورزش مورد علاقه 4 نفرا را از ورودی بگیرد و در یک دیکشنری ذخیره نماید
+# نفر سوم را از دیکشنری حذف نمایید
+# یک نفر جدید به دیکشنری اضافه کنید
+
+
+# برنامه ای بنویسید که نام و سن 4 دانش آموز  را از وزودی دریافت نماید و در یک دیکشنری ذخیره نماید
+# مجموع سن دانش آموزان را از داخل دیکشنری حساب نماید.
