5a9d225ab88a2c3dce8833c5833d88f7725ae3c8
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-22 19:01:08 +0330

    sadeghi

---
+++ about_loop.py
@@ -0,0 +1,32 @@
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# print('ok')
+# x = range(10)
+# print(x)
+
+# for _ in range(10):
+#     print('ok')
+#     print('balalalal')
+
+
+# name = "atay"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# # print(name[3])
+# for n in name:
+#     print(n)
+
+# exercise 1:
+# با کمک حلقه فور برنامه ای بنویسید که 10 عدد از ورودی دریافت نماید و حاصل جمع آن ها را محاسبه
+# و پرینت نماید
+
+
+# الگوریتم و فوچارت
