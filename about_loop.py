298fe3e35fb9ff71d6d2b75a92171b507d43f8d5
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-27 19:00:36 +0330

    sadeghi

--- about_loop.py
+++ about_loop.py
@@ -24,9 +24,24 @@
 # for n in name:
 #     print(n)
 
+# numbers = [1, 2, 3, 4, 5]
+# for x in numbers:
+#     print(x)
+
+
 # exercise 1:
 # با کمک حلقه فور برنامه ای بنویسید که 10 عدد از ورودی دریافت نماید و حاصل جمع آن ها را محاسبه
 # و پرینت نماید
 
+total = 0
+
+for i in range(10):
+    n = float(input('enter a number: '))
+    total = total + n
+print("total is:", total)
+
 
 # الگوریتم و فوچارت
+# exercise 1:
+# برنامه ای بنویسید که ده عدد را از ورودی دریافت کند و مجموع اعداد فرد را پرینت نماید
+# به همراه الگوریتم و فلوچارت
