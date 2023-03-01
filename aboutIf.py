32ff52111810a435adc0ed6f09a26d45a3b0ceb1
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-01 18:58:22 +0330

    sadeghi

---
+++ aboutIf.py
@@ -0,0 +1,41 @@
+a = 4
+b = 5
+if a < b:
+    print("a is lower than b.")
+if a > b:
+    print("a is greater than b.")
+a = 4
+b = 4
+if a <= b:
+    print("a is lower than or equal to b.")
+if a > b:
+    print("a is greater than b.")
+a = 4
+b = 4
+if a == b:
+    print("a is is equal to b.")
+a = 4
+b = 5
+if a != b:
+    print("a is is not equal to b.")
+if not a == b:
+    print("a is is not equal to b.")
+
+# exercise1:
+# برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید
+# در صورتی که عدد اول از دو عدد دیگر بزرگتر باشد
+# a is greater than b,c
+# در صورتی که عدد اول از دو عدد دیگر کوچکتر باشد:
+# a is lower than b,c
+# در صورتی که عدد اول از عدد دوم بزرگتر ولی از عدد سوم کوچکتر باشد:
+# a is greater tha nb but is lower than c
+# را نمایش دهد.
+
+a = 5
+b = 8
+
+if a == 5 and b == 7:
+    print("ok")
+
+if a == 5 or b == 7:
+    print("ok")
---
+++ review1.py
@@ -0,0 +1,29 @@
+# print("hello")
+# print("1+2*3-2")
+# print(1+2*3-2)
+# print("hi\nwelcome to our python class.")
+# print("hello\twelcome\tpython")
+# print("hi.\rwelcome to our python class.")
+# print("welcome to our python class.\rhi.")
+# print("hello every body.\rwelcome to our python class.\rwelcome to our pygame")
+
+# score = 0
+# print("my score is:", score)
+
+# score = score + 1
+# print("my score is:", score)
+# score += 1
+# print("my score is:", score)
+
+# score = score - 1
+# print("my score is:", score)
+
+# score -= 1
+# print("my score is:", score)
+
+s1 = int(input('enter first score: '))
+s2 = int(input('enter second score: '))
+s3 = int(input('enter third score: '))
+
+ave = (s1 + s2 + s3) / 3
+print("your average is :", ave)
