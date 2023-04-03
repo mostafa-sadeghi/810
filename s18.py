32e017622fea52575ff146168182bd4e213fa80b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-03 18:55:48 +0330

    sadeghi

---
+++ s18.py
@@ -0,0 +1,49 @@
+# for i in range(5):
+#     print("I want to learn pygame.")
+#     print("I want to program great games with python")
+#     print("I want to create a website with python")
+
+# print("ok")
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i, end=' ')
+
+# for i in range(10):
+#     print(i+1, end=' ')
+
+# print("even numbers are: ", end=' ')
+# for i in range(2,11,2):
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(5):
+#     print((i + 1) * 2, end=' ')
+
+# print()
+# print("odd numbers are: ", end=' ')
+# for i in range(1,11,2):
+#     print(i, end=' ')
+
+# numbers from 10 to 1
+# for i in range(10, 0, -1):
+#     print(i, end=' ')
+
+# for i in [2, 6, 4, 2, 4, 6, 7, 4]:
+#     print(i, end=' ')
+# print()
+# for i in (2, 6, 4, 2, 4, 6, 7, 4):
+#     print(i, end=' ')
+# print()
+# for i in '2357890':
+#     print(i, end=' ')
+# exercise:
+# یک برنامه بنویسید که یک عدد پنج رقمی را از ورودی دریافت نماید و مجموع ارقام آن را محاسبه و نمایش دهد.
+# یک برنامه بنویسید که یک عدد پنج رقمی را از ورودی دریافت نماید و مجموع ارقام زوج آن را محاسبه و نمایش دهد.
+# یک برنامه بنویسید که یک عدد پنج رقمی را از ورودی دریافت نماید و مجموع ارقام فرد آن را محاسبه و نمایش دهد.
