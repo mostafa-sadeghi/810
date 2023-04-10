376e3b6ed92d5c0cbe556815821490ebe7bed7ad
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-10 18:59:36 +0330

    sadeghi

--- s18.py
+++ s18.py
@@ -54,12 +54,12 @@
 #     result += int(n) # result = result + int(n)
 
 # print(result)
-sum_of_even_digits = 0
-sum_of_odd_digits = 0
-number = input('enter a number:> ')
-for n in number:
-    if int(n) % 2 == 0:
-        # sum_of_even_digits = sum_of_even_digits + int(n)
+# sum_of_even_digits = 0
+# sum_of_odd_digits = 0
+# number = input('enter a number:> ')
+# for n in number:
+#     if int(n) % 2 == 0:
+# sum_of_even_digits = sum_of_even_digits + int(n)
 #         sum_of_even_digits += int(n)
 #     else:
 #         sum_of_odd_digits += int(n)
@@ -76,3 +76,35 @@ for n in number:
 # 4. n = n // 10
 # 5 . if   n!= 0 goto step 2, else: goto step 6
 # 6. end
+
+
+#   مجموع رقم های زوج و فرد یک عدد به صورت جداگانه
+
+# n = int(input('enter a "number": '))
+# sum_of_even_digits = 0
+# sum_of_odd_digits = 0
+# while n:
+#     m = n % 10
+#     if m % 2 == 0:
+#         sum_of_even_digits += m  # sum_of_even_digits = sum_of_even_digits + m
+#     else:
+#         sum_of_odd_digits += m
+#     n //= 10  # n = n // 10
+# print("sum of even digits :", sum_of_even_digits)
+# print("sum of odd digits :", sum_of_odd_digits)
+
+
+#
+
+'''
+برنامه ای برای ثبت نام دانش آموزان مدرسه خود بنویسید
+در این برنامه اطلاعات دانش آموز از ورودی دریافت می شود
+و در قالب دیکشنری یک لیست اضافه می شود
+
+برنامه امکان اضافه کردن
+حذف کردن و ویرایش اطلاعات دانش آموزان را دارد
+همینطور امکان نمایش لیست تمام اطلاعات دانش آموزان مدرسه را دارد
+
+
+
+'''
